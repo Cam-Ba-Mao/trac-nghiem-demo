33989437eec853d11ef2f2e6687d9e32634bf5f5
BaMao <cambamao98@gmail.com>
2024-07-08 00:07:15 +0700

    update frontend login page

--- header.php
+++ header.php
@@ -6,11 +6,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="<?php echo BASE_URL; ?>/assets/img/Icon.png">
     <title><?php echo isset($title) ? $title : "Website thi trắc nghiệm"; ?></title>
     <script src="<?php echo BASE_URL; ?>/assets/js/jquery-3.7.1.min.js"></script>
     <script src="<?php echo BASE_URL; ?>/assets/js/bootstrap.min.js"></script>
     <script src="<?php echo BASE_URL; ?>/assets/js/scripts.js"></script>
     <!-- <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/cssreset.css" /> -->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/fonts/material-icon/css/material-design-iconic-font.min.css"/>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/bootstrap.min.css"/>
     <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/assets/css/styles.css">
 </head>
@@ -19,7 +21,7 @@
         <!-- <h1>Quiz Application</h1> -->
         <div class="tdmu-header__wrap">
             <div class="tdmu-header__brand">
-                <a href="http://">
+                <a href="<?php echo BASE_URL; ?>/index.php">
                     <img src="<?php echo BASE_URL; ?>/assets/img/Logo_TDMU_2024_nguyen_ban.svg" alt="">
                 </a>
             </div>
